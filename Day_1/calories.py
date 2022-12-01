732fe970270daf960d5d150cd4cf9a050fb46c87
Julio Emanuel Ruzicka <71849641+cracksuxer@users.noreply.github.com>
2022-12-01 11:20:41 +0000

    Changed to functional programming

--- Day_1/calories.py
+++ Day_1/calories.py
@@ -1,17 +1,21 @@
 import os
 def main():
-    lines = []
-    sum = 0
+    elves_calories = []
+    calories_sum = 0
+
     f = open("input.txt", "r")
+
     for line in f:
         if line == '\n':
-            lines.append(sum)
-            sum = 0
+            elves_calories.append(calories_sum)
+            calories_sum = 0
         else:
             line_copy = line.rstrip('\n')
-            sum += int(line_copy)
-    lines.sort(reverse=True)
-    best_3 = lines[0] + lines[1] + lines[2]
+            calories_sum += int(line_copy)
+            
     f.close()
-    print(best_3)
+    three_best_sum = sum(sorted(elves_calories, reverse=True)[:3])
+    print(three_best_sum)
+
+
 main()

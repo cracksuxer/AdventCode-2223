1d9ecea0f099caf1b464567c88453a2b6c443dcf
cracksuxer <julioemanuelruzicka2esoc@gmail.com>
2022-12-07 11:53:39 +0000

    Prettier

--- Day_6/tuning.py
+++ Day_6/tuning.py
@@ -1,4 +1,7 @@
 from rich.console import Console
+from rich.traceback import install
+
+install()
 
 def main():
     console = Console()
@@ -9,17 +12,15 @@ def main():
     file.close()
 
     for packet in packets:
-        start = 0
-        first_four = ""
+        start, packet_len = 0, 0
         packet_len = 0
         while packet_len != 14:
             # ------ PART 1 ------ #
             # first_four = packet[start:start+4]
             # ------ PART 2 ------ #
-            first_14 = packet[start:start+14]
-            packet_len = len(dict.fromkeys(first_14))
+            packet_len = len(dict.fromkeys(packet[start:start+14]))
             start += 1
-        console.print("First 14 unique characters starts at {} letter: [green]{}[/]".format(start+13, first_four))
+        console.print("First 14 unique characters starts at {} letter[/]".format(start+13))
 
 if __name__ == "__main__":
     main()

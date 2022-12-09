d6422e112fedb932c24cb533af5e8afa1d9dae7c
cracksuxer <julioemanuelruzicka2esoc@gmail.com>
2022-12-09 00:19:22 +0000

    Prettier

--- Day_7/bash.py
+++ Day_7/bash.py
@@ -11,12 +11,6 @@ class File:
         self._size = size
         self._parent = parent
 
-    def __str__(self):
-        return f"File: {self.name}, Size: {self.size}"
-
-    def __repr__(self):
-        return f"File: {self.name}, Size: {self.size}"
-
     @property
     def name(self):
         return self._name
@@ -26,15 +20,6 @@ class File:
     @property
     def parent(self):
         return self._parent
-    @name.setter
-    def name(self, name):
-        self._name = name
-    @size.setter
-    def size(self, size):
-        self._size = size
-    @parent.setter
-    def parent(self, parent):
-        self._parent = parent
 
 class Folder:
     def __init__(self, name: str, subfolders: list, path: str, parent: 'Folder', size : int = 0) -> None:
@@ -43,12 +28,6 @@ class Folder:
         self._path = path
         self._size = size
         self._parent = parent
-
-    def __str__(self) -> str:
-        return f"Folder: {self.name}, Size: {self.size}, Subfolders: {self.subfolders}"
-
-    def __repr__(self) -> str:
-        return f"Folder: {self.name}, Size: {self.size}, Subfolders: {self.subfolders}"
 
     def tree_print(self, level: int = 0) -> None:
         parent_name = self.parent.name if self.parent else "None"
@@ -59,18 +38,6 @@ class Folder:
             else:
                 parent_name = i.parent.name if i.parent else "None"
                 print(f"{'    ' * (level + 1)}[bold green]{i.name}[/] ({i.size}) <- [yellow]{parent_name}[/]")
-        
-    def search_folder(self, name: str) -> 'Folder':
-        if self.name == name:
-            return self
-        for i in self.subfolders:
-            if isinstance(i, Folder):
-                if i.name == name:
-                    return i
-                else:
-                    return i.search_folder(name)
-            else:
-                return None
 
     def search_by_path(self, path: str) -> 'Folder':
         if path == '/':
@@ -87,24 +54,17 @@ class Folder:
                         return i.search_by_path("/" + ''.join(path.split('/', 2)[2:]))
         
         raise Exception(f"Folder {key_to_find} not found")
-            
-    def add_folder(self, folder: 'Folder') -> None:
-        self.subfolders.append(folder)
-        self.size = self.calc_size()
 
-        parent = self.parent
-        while parent:
-            parent.size += folder.size
-            parent = parent.parent
-
-    def add_file(self, file) -> None:
-        self.subfolders.append(file)
-        self.size = self.calc_size()
-
-        parent = self.parent
-        while parent:
-            parent.size += file.size
-            parent = parent.parent
+    def add(self, item) -> None:
+        self.subfolders.append(item)
+        try:
+            self.size = self.calc_size()
+            parent = self.parent
+            while parent:
+                parent.size += item.size
+                parent = parent.parent
+        except AttributeError:
+            pass
 
     def calc_size(self) -> int:
         total_size = 0
@@ -133,24 +93,10 @@ class Folder:
     @size.setter
     def size(self, size: int) -> None:
         self._size = size
-    @path.setter
-    def path(self, path: str) -> None:
-        self._path = path
-    @name.setter
-    def name(self, name: str) -> None:
-        self._name = name
-    @subfolders.setter
-    def subfolders(self, subfolders: list) -> None:
-        self._subfolders = subfolders
-    @parent.setter
-    def parent(self, parent: 'Folder') -> None:
-        self._parent = parent
-
 
 def main():
     directory = Folder("/", [], "/", None)
     current_dir = ""
-    flag_ls = False
 
     file = open("input.txt", "r")
 
@@ -158,7 +104,6 @@ def main():
         line_s = line.strip().split()
         if line.startswith("$"):
             if line_s[1] == 'cd':
-                flag_ls = False
                 if line_s[2] == '..':
                     current_dir = current_dir.rsplit('/', 1)[0]
                     if current_dir == '':
@@ -170,17 +115,27 @@ def main():
                         current_dir += line_s[2]  
                     else: 
                         current_dir += "/" + line_s[2]
-            elif line_s[1] == 'ls':
-                flag_ls = True
-        elif flag_ls == True:
-            if line_s[0] == "dir":
-                directory.search_by_path(current_dir).add_folder(Folder(line_s[1], [], current_dir, directory.search_by_path(current_dir)))
-            else :
-                directory.search_by_path(current_dir).add_file(File(line_s[1], directory.search_by_path(current_dir), int(line_s[0])))
+        elif line_s[0] == "dir":
+            directory.search_by_path(current_dir).add(Folder(line_s[1], [], current_dir, directory.search_by_path(current_dir)))
+        else :
+            directory.search_by_path(current_dir).add(File(line_s[1], directory.search_by_path(current_dir), int(line_s[0])))
 
     file.close()
-    directory.tree_print()
+    # directory.tree_print()
 
+    # ----- PART 1 ----- #
+    # def find_folders(d: Folder, cap: int = 100000) -> list:
+    #     folders = []
+    #     for i in d.subfolders:
+    #         if isinstance(i, Folder):
+    #             if i.size <= cap:
+    #                 folders.append(i.size)
+    #             folders += find_folders(i, cap)
+    #     return folders
+
+    # print(f"Sum of folders with size <= 100000: {sum(find_folders(directory))}")
+
+    # ----- PART 2 ----- #
     total_space = directory.size
     max_space = 70000000
     space_needed = 30000000
@@ -196,7 +151,7 @@ def main():
                 folders += list_folders(i, update)
         return folders
 
-    console.log(min(list_folders(directory, need_to_delete), key=lambda x:abs(x-6876531)))
+    print(f"Minimum folder size to fit the update: {min(list_folders(directory, need_to_delete), key=lambda x:abs(x-6876531))}")
     
 if __name__ == "__main__":
     main()
